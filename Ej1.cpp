102a67b96c5eeaf7d5b1af95dd3998c36b548f32
Juan Miguel Acosta Ortega <100489090+JuanmiAcosta@users.noreply.github.com>
2022-11-09 09:36:44 +0100

    Add files via upload

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,11 +1,16 @@
-#include<iostream>
-#include<stack>
-#include<list>
-#include<algorithm>
+#include <iostream>
+#include <stack>
+#include <list>
+#include <algorithm>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
-//bool myfunction (int i,int j) { return (i<j); } //Este sería el funtor a aniadir, pero justo es el de por defecto
+bool compareTops (const stack<int> &first, const stack<int> &second){
+	return (first.top() < second.top());	
+}
+
 
 void Borrar(list<stack<int> > & L, int elemento){ // el método borra el top de las pilas de enteros que coincidad con elemento y ordena de menor a mayor la lista (por defecto)
 	list<stack<int> >::iterator it;
@@ -17,19 +22,17 @@ void Borrar(list<stack<int> > & L, int elemento){ // el método borra el top de l
 		}				
 	}
 	
-	L.sort();	
+	L.sort(); 
 }
 
 ostream& operator<<(ostream &flujo, const stack<int> &pila){ // Sobrecargamos el operador "<<" para que muestre solo el top de las pilas de enteros de la lista
-	
-	flujo << pila.top() << "\n";
-    
+	if (pila.size() != 0){
+		flujo << pila.top() << "\n";		
+	}
     return flujo;
 }
 
 int main (){
-	
-	const int MAX_1=2, MAX_2=4, MAX_3=6;
 	
 	list<stack<int> > lista;
 	list<stack<int> >::iterator it; // Generamos un iterador sobre la estructura lineal "list" lista para recorrerla
@@ -37,42 +40,58 @@ int main (){
 	stack<int> pila1;
 	stack<int> pila2;
 	stack<int> pila3;
+	stack<int> pila4;
+	stack<int> pila5;
+	stack<int> pila6;
 	
+	pila1.push(1);
+	pila1.push(9);
+	pila1.push(2);
 	
-	for (int i=0; i<MAX_1; i++){
-		pila1.push(i);
-	}
+	pila2.push(0);
+	pila2.push(1);
+	pila2.push(2);
 	
-	for (int i=8; i>MAX_2; i--){
-		pila2.push(i);
-	}
+	pila3.push(2);
 	
-	for (int i=0; i<MAX_3; i++){ //Creamos tres pilas de ejemplo para ver si funciona el programa
-		pila3.push(i);
-	}
+	pila4.push(7);
+	pila4.push(10);
+	pila4.push(9);
+	pila4.push(3);
+	
+	pila5.push(2);
+	pila5.push(4);
+	
+	pila6.push(2);
+	pila6.push(8);
 	
 	it = lista.begin(); //Colocamos el iterador al principio de la lista
 	
-	lista.push_back(pila1); // 0 , 1 (top 1)
-	lista.push_back(pila2); // 8 , 7 , 6 , 5 (top 5)
-	lista.push_back(pila3); // 0 , 1 , 2 , 3 , 4 , 5 (top 5)
+	lista.push_back(pila1); 
+	lista.push_back(pila2); 
+	lista.push_back(pila3);
+	lista.push_back(pila4);
+	lista.push_back(pila5);
+	lista.push_back(pila6);
 	
 	cout << "Los top sin orden son: \n";
 	
-	for (it=lista.begin(); it!=lista.end(); ++it){ // 1 , 5 , 5
-		cout << ' ' << *it;
+	for (it=lista.begin(); it!=lista.end(); ++it){ 
+		cout << "\t" << *it << endl;
 	}
 	
 	cout << "\n";
-	cout << "El elemento a borrar es el 5\n"; //Borramos los top que sean 5 y ordenamos de menor a mayor
+	cout << "El elemento a borrar es el 2\n"; //Borramos los top que sean 2 y ordenamos de menor a mayor
 	
-	Borrar(lista, 5); // 1, 6 , 4 y se ordena 1 , 4 , 6
-
+	Borrar(lista, 2); 
 	
-	cout << "Borrado los top 5; Los top ordenados son: \n";
+	cout << "Borrado los top 2; \nLos top ordenados son: \n";
+	cout << endl;
 	
-	for (it=lista.begin(); it!=lista.end(); ++it){ // 1 , 4 , 6
-		cout << ' ' << *it;
+	for (it=lista.begin(); it!=lista.end(); ++it){
+		cout << "\t" << *it << endl;
 	}
+	
+	return(0);
 	
 }
