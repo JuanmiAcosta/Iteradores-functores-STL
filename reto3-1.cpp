f8d684bc39a2a336adc75e3624dde93fbb21049a
Juan Miguel Acosta Ortega <100489090+JuanmiAcosta@users.noreply.github.com>
2022-11-18 16:01:05 +0100

    Add files via upload

--- reto3-1.cpp
+++ reto3-1.cpp
@@ -3,16 +3,39 @@
 #include <list>
 #include <algorithm>
 
+/**
+* @file reto3-1.cpp
+* @Author Juan Miguel Acosta Ortega y Luis Soto Torres
+* @date 18/11/2022
+* @brief Reto 3.1 (Borrar y ordenar una lista de pilas de enteros)
+*/
+
 using namespace std;
 
-bool myfunction (stack<int> &i,stack<int> &j) { 
+/**
+      @brief El functor que solapará al que tiene por defecto el método sort de list, ordenará las pilas de menor a mayor según sus top.
+	  @param <em>i</em> 1ª pila.
+      @param <em>j</em> 2ª pila.
+      @return Devuelve un booleano que indica cuál es el top menor.
+*/
+
+bool ordenaTopMenorMayor (stack<int> &i,stack<int> &j) { 
 	if(!i.empty()&&!j.empty()){
 		return (i.top()<j.top());	
 	}
 	return (-1); 
-} //Este serï¿½a el funtor a aniadir, pero justo es el de por defecto
+} 
 
-void Borrar(list<stack<int> > & L, int elemento){ // el mï¿½todo borra el top de las pilas de enteros que coincidad con elemento y ordena de menor a mayor la lista (por defecto)
+/**
+      @brief Borra los top de la lista que sea igual al elemento pasado, acto seguido ordena la lista según un functor pasado al método sort de list.
+	  @param <em>L</em> es la lista de pilas de enteros con la que se trabajará.
+      @param <em>elemento</em> es el entero a borrar de los top de las pilas de la lista.
+      @pre No pasa nada si alguna pila se vacía, se controlará en el método
+      @post Borra los top de las pilas que sean iguales al segundo parámetro pasado, y acto seguido ordena las pilas de menor a mayor según sus top.
+      @return No devuelve una copia, trabaja con las pilas de la lista por referencia y no por valor.
+*/
+
+void Borrar(list<stack<int> > & L, int elemento){ 
 	list<stack<int> >::iterator it;
 	list<stack<int> >::iterator it2;
 	it = L.begin();
@@ -26,8 +49,15 @@ void Borrar(list<stack<int> > & L, int elemento){ // el mï¿½todo borra el top de
 		}				
 	}
 		
-	L.sort(myfunction);	
+	L.sort(ordenaTopMenorMayor);	
 }
+
+/**
+      @brief Sobrecarga del operador de salida para mostrar las pilas según su top por la salida estándar.
+	  @param <em>flujo</em> es el flujo del operador de salida.
+      @param <em>pila</em> la pìla de la que se "extraerá" el top.
+      @return Devuelve por referencia el flujo del operador de salida.
+*/
 
 ostream& operator<<(ostream &flujo, const stack<int> &pila){ // Sobrecargamos el operador "<<" para que muestre solo el top de las pilas de enteros de la lista
 	stack<int> aux=pila;
@@ -39,6 +69,10 @@ ostream& operator<<(ostream &flujo, const stack<int> &pila){ // Sobrecargamos el
     
     return flujo;
 }
+
+/**
+      @brief En el main simplemente llenamos una lista de pilas de enteros con las pilas del ejemplo y llamammos a la función Borrar() correspondiente.
+*/
 
 int main (){
 	
@@ -64,27 +98,27 @@ int main (){
 
 	it = lista.begin(); //Colocamos el iterador al principio de la lista
 	
-	lista.push_back(pila1); // 0 , 1 (top 1)
-	lista.push_back(pila2); // 8 , 7 , 6 , 5 (top 5)
-	lista.push_back(pila3); // 0 , 1 , 2 , 3 , 4 , 5 (top 5)
+	lista.push_back(pila1); 
+	lista.push_back(pila2); 
+	lista.push_back(pila3); 
 	lista.push_back(pila4);
 	lista.push_back(pila5);
 	lista.push_back(pila6);
 	cout << "La lista es: \n";
 	
-	for (it=lista.begin(); it!=lista.end(); ++it){ // 1 , 5 , 5
+	for (it=lista.begin(); it!=lista.end(); ++it){ 
 		cout << ' ' << (*it);
 	}
 	
 	cout << "\n";
-	cout << "El elemento a borrar es el 2\n"; //Borramos los top que sean 5 y ordenamos de menor a mayor
+	cout << "El elemento a borrar es el 2\n"; //Borramos los top que sean 2 y ordenamos de menor a mayor
 	
-	Borrar(lista, 2); // 1, 6 , 4 y se ordena 1 , 4 , 6
+	Borrar(lista, 2); 
 
 	
-	cout << "Borrado los top 5; Los top ordenados son: \n";
+	cout << "Borrado los top 2; Los top ordenados son: \n";
 	
-	for (it=lista.begin(); it!=lista.end(); ++it){ // 1 , 4 , 6
+	for (it=lista.begin(); it!=lista.end(); ++it){ 
 		cout << ' ' << *it;
 	}
 	
